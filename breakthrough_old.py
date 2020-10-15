ae071dad836b5d0a4858efa4c53e1a4d44aeb926
Matteo Belenchia <matteo.belenchia@studenti.unicam.it>
2020-10-15 16:13:58 +0000

    new breakthrough

--- breakthrough_old.py
+++ breakthrough_old.py
@@ -14,22 +14,10 @@ class Breakthrough:
             assert (rows >= 4)
             self._cols = cols
             self._rows = rows
-            self._size = cols * rows
             self._board = [self.NoPce for _ in range(self._rows*self._cols)]
             self._hash_key = 0
             self._hash_board = [[random.getrandbits(64) for _ in range(self._rows*self._cols)] for _ in range(2)]
-            self.d_nw = cols - 1
-            self.d_n = cols
-            self.d_ne = cols + 1
-            self.d_e = 1
-            self.d_se = -cols + 1
-            self.d_s = -cols
-            self.d_sw = -cols - 1
-            self.d_w = -1
             return
-
-        def size(self):
-            return self._size
 
         def cols(self):
             return self._cols
@@ -38,20 +26,17 @@ class Breakthrough:
             return self._rows
 
         def sqr(self, c, r):
-            # assert(0 <= c < self.cols())
-            # assert(0 <= r < self.rows())
-            return r * self._cols + c
+            assert(0 <= c < self.cols())
+            assert(0 <= r < self.rows())
+            return r * self.cols() + c
 
         def col(self, sqr):
-            # assert(0 <= sqr < self.rows() * self.cols())
-            return sqr % self._cols
+            assert(0 <= sqr < self.rows() * self.cols())
+            return sqr % self.cols()
 
         def row(self, sqr):
-            # assert(0 <= sqr < self.rows() * self.cols())
-            return sqr // self._cols
-
-        def col_row(self, sqr):
-            return sqr % self._cols, sqr // self._cols
+            assert(0 <= sqr < self.rows() * self.cols())
+            return sqr // self.cols()
 
         def clear(self):
             self._board = [self.NoPce for _ in range(self._rows*self._cols)]
@@ -59,7 +44,7 @@ class Breakthrough:
             return
 
         def set(self, sqr, pce):
-            # assert(0 <= sqr < self.rows() * self.cols())
+            assert (0 <= sqr < self.rows() * self.cols())
             if self._board[sqr] != self.NoPce:
                 self._hash_key ^= self._hash_board[self._board[sqr]][sqr]
             if pce != self.NoPce:
@@ -68,28 +53,21 @@ class Breakthrough:
             return
 
         def set2d(self, c, r, pce):
-            # assert (0 <= c < self.cols())
-            # assert (0 <= r < self.rows())
+            assert (0 <= c < self.cols())
+            assert (0 <= r < self.rows())
             self.set(self.sqr(c, r), pce)
 
         def get(self, sqr):
-            # assert(0 <= sqr < self.rows() * self.cols())
+            assert(0 <= sqr < self.rows() * self.cols())
             return self._board[sqr]
 
         def get2d(self, c, r):
-            # assert(0 <= c < self.cols())
-            # assert(0 <= r < self.rows())
-            return self._board[r * self._cols + c]
+            assert(0 <= c < self.cols())
+            assert(0 <= r < self.rows())
+            return self.get(self.sqr(c, r))
 
         def get_key(self):
             return self._hash_key
-
-        def get_pce_locations(self, p):
-            pce_loc = []
-            for sqr in range(self._size):
-                if self._board[sqr] == p:
-                    pce_loc.append(sqr)
-            return pce_loc
 
     def __init__(self, cols, rows):
         self._board = self.Board(cols, rows)
@@ -148,42 +126,35 @@ class Breakthrough:
 
     def generate(self, shuffle=False):
         moves = []
-        board = self._board
         if self._to_move == self.White:
-            for s in reversed(board.get_pce_locations(self.White)):
-                c, r = board.col_row(s)
-                if c > 0:
-                    s_to = s + board.d_nw
-                    if board.get(s_to) != self.White:
-                        moves.append((s, s_to, self._board.get(s_to)))
-                s_to = s + board.d_n
-                if board.get(s_to) == self._board.NoPce:
-                    moves.append((s, s_to, board.NoPce))
-                if c < board.cols() - 1:
-                    s_to = s + board.d_ne
-                    if board.get(s_to) != self.White:
-                        moves.append((s, s_to, board.get(s_to)))
+            for r in range(self._board.rows()-1):
+                for c in range(self._board.cols()):
+                    if self._board.get2d(c, r) == self.White:
+                        if c > 0 and self._board.get2d(c-1, r+1) != self.White:
+                            moves.append([self._board.sqr(c, r), self._board.sqr(c-1, r+1), self._board.NoPce])
+                        if self._board.get2d(c, r+1) == self._board.NoPce:
+                            moves.append([self._board.sqr(c, r), self._board.sqr(c, r+1), self._board.NoPce])
+                        if c < self._board.cols() - 1 and self._board.get2d(c+1, r+1) != self.White:
+                            moves.append([self._board.sqr(c, r), self._board.sqr(c+1, r+1), self._board.NoPce])
         else:
-            for s in board.get_pce_locations(self.Black):
-                c, r = board.col_row(s)
-                if c > 0:
-                    s_to = s + board.d_sw
-                    if board.get(s_to) != self.Black:
-                        moves.append((s, s_to, self._board.get(s_to)))
-                s_to = s + board.d_s
-                if board.get(s_to) == self._board.NoPce:
-                    moves.append((s, s_to, board.NoPce))
-                if c < board.cols() - 1:
-                    s_to = s + board.d_se
-                    if board.get(s_to) != self.Black:
-                        moves.append((s, s_to, board.get(s_to)))
+            for r in range(self._board.rows()-1, 0, -1):
+                for c in range(self._board.cols()):
+                    if self._board.get2d(c, r) == self.Black:
+                        if c > 0 and self._board.get2d(c-1, r-1) != self.Black:
+                            moves.append([self._board.sqr(c, r), self._board.sqr(c-1, r-1), self._board.NoPce])
+                        if self._board.get2d(c, r-1) == self._board.NoPce:
+                            moves.append([self._board.sqr(c, r), self._board.sqr(c, r-1), self._board.NoPce])
+                        if c < self._board.cols() - 1 and self._board.get2d(c+1, r-1) != self.Black:
+                            moves.append([self._board.sqr(c, r), self._board.sqr(c+1, r-1), self._board.NoPce])
         if shuffle:
             random.shuffle(moves)
         return moves
 
     def make(self, move):
         sqr_from, sqr_to, _ = move
-        if self._board.get(sqr_to) != self._board.NoPce:
+        pce = self._board.get(sqr_to)
+        move[2] = pce
+        if pce != self._board.NoPce:
             self._pce_count[self.Opponent[self._to_move]] -= 1
         self._board.set(sqr_to, self._board.get(sqr_from))
         self._board.set(sqr_from, self._board.NoPce)
